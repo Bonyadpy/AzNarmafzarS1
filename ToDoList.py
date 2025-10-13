3bc48b4ec8ae570031fb640fc41630c5407aa507
Bonyadpy <programming4ever1381@gmail.com>
2025-10-12 18:46:30 -0700

    To Do List v.3

--- ToDoList.py
+++ ToDoList.py
@@ -1,17 +1,22 @@
 import tkinter as tk
 from tkinter import messagebox
 from datetime import datetime
+from tkinter import ttk
+
 
 
 def add_task():
     task = task_entry.get()
+    category = category_var.get()
     if task != "":
+        from datetime import datetime
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M")
-        task_with_time = f"{task} - [{current_time}]"
+        task_with_time = f"[{category}] {task}  ({current_time})"
         tasks_listbox.insert(tk.END, task_with_time)
         task_entry.delete(0, tk.END)
     else:
         messagebox.showwarning("Warning", "لطفاً یک تسک وارد کنید!")
+
 
 def mark_done(): 
     try:
@@ -39,14 +44,26 @@ def clear_all():
 
 root = tk.Tk()
 root.title("To-Do List v1 - Basic")
-root.geometry("400x400")
+root.geometry("500x500")
 root.resizable(False, False)
 
 task_entry = tk.Entry(root, width=25, font=('Arial', 14))
 task_entry.pack(pady=10)
 
-add_button = tk.Button(root, text="Add Task", width=10, bg="green", fg="white", font=('Arial', 10, 'bold'), command=add_task)
-add_button.pack()
+top_frame = tk.Frame(root)
+top_frame.pack(pady=10)
+
+task_entry = tk.Entry(top_frame, width=25, font=('Arial', 14))
+task_entry.grid(row=0, column=0, padx=5)
+
+categories = ["General", "Home", "Study", "Shopping", "Work"]
+category_var = tk.StringVar(value=categories[0])
+category_menu = ttk.Combobox(top_frame, textvariable=category_var, values=categories, width=10, state="readonly")
+category_menu.grid(row=0, column=1, padx=5)
+
+add_button = tk.Button(top_frame, text="Add Task", width=10, bg="green", fg="white", font=('Arial', 10, 'bold'), command=add_task)
+add_button.grid(row=0, column=2, padx=5)
+
 
 tasks_listbox = tk.Listbox(root, width=50, height=10, selectmode=tk.SINGLE, font=('Arial', 12))
 tasks_listbox.pack(pady=10)
